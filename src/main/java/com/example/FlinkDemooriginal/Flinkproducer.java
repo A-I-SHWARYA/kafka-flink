3e7b6840dd24de2d616313ef5edfc845367a876f
Aishwarya Murali <Aishwarya@NTP-LAP-1173.chn.nestgroup.net>
2023-02-01 23:57:04 +0530

    done

--- src/main/java/com/example/FlinkDemooriginal/Flinkproducer.java
+++
@@ -1,5 +0,0 @@
-package com.example.FlinkDemooriginal;
-
-public class Flinkproducer {
-
-}
--- src/main/java/com/example/FlinkDemooriginal/Flinkreceiver.java
+++ src/main/java/com/example/FlinkDemooriginal/Flinkreceiver.java
@@ -6,12 +6,12 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 
 import java.util.Properties;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
+@SpringBootApplication
 public class Flinkreceiver {
     static StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
 
 
     public static FlinkKafkaConsumer011<String>
@@ -20,24 +20,29 @@ public class Flinkreceiver {
 
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", kafkaAddress);
-//        props.setProperty("group.id",kafkaGroup);
+        props.setProperty("group.id","kafka-streams-demo");
         FlinkKafkaConsumer011<String> consumer = new FlinkKafkaConsumer011<>(
                 topic, new SimpleStringSchema(), props);
 
         // Create a DataStream from the consumer
         DataStream<String> stream = env.addSource(consumer);
 
+
         // Print the consumed strings
         stream.print();
 
         return consumer;
     }
+
+
     public static void main(String[] args) throws Exception {
+        SpringApplication.run(Flinkreceiver.class, args);
         Flinkreceiver receiver = new Flinkreceiver();
         receiver.createStringConsumerForTopic("output-topic", "kafka:9092");
-        env.execute();
+          env.execute();
+
+
     }
 
 
 }
-
